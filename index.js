deaf4bfd2761d7f8bf802741f570d770d9a0834f
TajoDag <taodx.dev@gmail.com>
2024-06-09 20:55:11 +0700

    fix nhieu

--- index.js
+++ index.js
@@ -8,7 +8,8 @@ const server = http.createServer(app);
 
 const io = new Server(server, {
   cors: {
-    origin: [process.env.CLIENT_PORT, process.env.ADMIN_PORT],
+    // origin: [process.env.CLIENT_PORT, process.env.ADMIN_PORT],
+    origin: "*",
     methods: ["GET", "POST"],
   },
 });
